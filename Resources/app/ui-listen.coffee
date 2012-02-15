a6ca008996a61e1fa34e745c80722265761b401b
nikolaiwarner <n@nikolaiwarner.com>
2012-02-15 16:42:21 -0500

    back to working

--- Resources/app/ui-listen.coffee
+++ Resources/app/ui-listen.coffee
@@ -4,98 +4,112 @@ app.ui.createListenWindow = (options) ->
   options.navBarHidden = true
   
   window = new Window(options)
-  audio_stream = Titanium.Media.createAudioPlayer()
+  window.audio_stream = Titanium.Media.createAudioPlayer()
    
-  now_playing_track = new Track()
+  window.now_playing_track = new Track()
   
   
   
-  playing = false
-  now_playing_data_timer = undefined
-  now_playing_data_client = Titanium.Network.createHTTPClient()
+  window.playing = false
+  window.now_playing_data_timer = undefined
+  window.now_playing_data_client = Titanium.Network.createHTTPClient()
   
   
-  artist_text = Titanium.UI.createLabel
+  window.artist_text = Titanium.UI.createLabel
     className: "track_label"
   
-  album_text = Titanium.UI.createLabel
+  window.album_text = Titanium.UI.createLabel
     className: "track_label"
       
-  track_text = Titanium.UI.createLabel
+  window.track_text = Titanium.UI.createLabel
     className: "track_label"    
 
-  artwork_imageview = Titanium.UI.createImageView
+  window.artwork_imageview = Titanium.UI.createImageView
     top: 10
     left: 30
     width: 260
     height: 260
   
-  play_button = Titanium.UI.createImageView
+  window.play_button = Titanium.UI.createImageView
     top: 350
     left: 10
     width: 50
     height: 50
     image: "assets/images/playbutton.png"
   
-  refresh_now_playing_data = ->
-    self = this
+  
+  
+  
+  window.refresh_now_playing_data = ->
     url = "http://nwarner.com/projects/sandbox/now_playing.json"
-    now_playing_data_client.onload = refresh_now_playing_data_success
-    now_playing_data_client.onerror = refresh_now_playing_data_error
-    now_playing_data_client.open "GET", url
-    now_playing_data_client.send()
-    clearInterval now_playing_data_timer
-    now_playing_data_timer = setTimeout(->
-      refresh_now_playing_data()
+    
+    Ti.ajax
+      url: url
+      success: (response) ->
+        #warn response
+        #response = JSON.parse(response)
+        window.now_playing_track = response.track
+        log window.now_playing_track
+        window.update_ui()
+
+      error: (response) ->
+        alert "now_playing_track response error!"    
+    
+    clearInterval window.now_playing_data_timer
+    window.now_playing_data_timer = setTimeout(->
+      window.refresh_now_playing_data()
     , 20000)
   
-  refresh_now_playing_data_success = ->
-    response = JSON.parse(now_playing_data_client.responseText)
-    now_playing_track = response.track
-    update_ui()
   
-  refresh_now_playing_data_error = ->
   
-  update_ui = ->
-    artist_text.text = now_playing.artist
-    album_text.text = now_playing.album
-    track_text.text = now_playing.title
-    artwork_imageview.image = "assets/images/album_bg.png"
   
-  play_stream = ->
-    playing = true
-    audio_stream.setUrl stream_url
-    audio_stream.start()
-    play_button.image = "assets/images/stopbutton.png"
+  window.update_ui = ->
+    window.artist_text.text = window.now_playing.artist
+    window.album_text.text = window.now_playing.album
+    window.track_text.text = window.now_playing.title
+    window.artwork_imageview.image = "assets/images/album_bg.png"
   
-  stop_stream = ->
-    playing = false
-    audio_stream.stop()
-    play_button.image = "assets/images/playbutton.png"
   
-  toggle_play_stream = ->
-    if playing
-      stop_stream()
+  
+  window.play_stream = ->
+    window.playing = true
+    window.audio_stream.setUrl app.config.urls.stream
+    window.audio_stream.start()
+    window.play_button.image = "assets/images/stopbutton.png"
+  
+  
+  
+  window.stop_stream = ->
+    window.playing = false
+    window.audio_stream.stop()
+    window.play_button.image = "assets/images/playbutton.png"
+  
+  
+  
+  window.toggle_play_stream = ->
+    if window.playing
+      window.stop_stream()
     else
-      play_stream()
+      window.play_stream()
   
-  window.add artist_text
-  window.add album_text
-  window.add track_text
-  window.add artwork_imageview
-  window.add play_button
-  play_button.addEventListener "click", ->
-    toggle_play_stream()
-  
-  
-  
-  
-  
-  
+
   
   window.update = ->
-    refresh_now_playing_data()
+    window.refresh_now_playing_data()
 
+
+  
+  window.add window.artist_text
+  window.add window.album_text
+  window.add window.track_text
+  window.add window.artwork_imageview
+  window.add window.play_button
+  
+  $(window.play_button).click ->
+    window.toggle_play_stream()
+  
+  
+  
   
   window.addEventListener "focus", ->
     window.update()
--- Resources/app/ui-listen.js
+++ Resources/app/ui-listen.js
@@ -1,92 +1,91 @@
 
 app.ui.createListenWindow = function(options) {
-  var album_text, artist_text, artwork_imageview, audio_stream, now_playing_data_client, now_playing_data_timer, now_playing_track, play_button, play_stream, playing, refresh_now_playing_data, refresh_now_playing_data_error, refresh_now_playing_data_success, stop_stream, toggle_play_stream, track_text, update_ui, window;
+  var window;
   options = options || {};
   options.title = options.title || "Listen";
   options.navBarHidden = true;
   window = new Window(options);
-  audio_stream = Titanium.Media.createAudioPlayer();
-  now_playing_track = new Track();
-  playing = false;
-  now_playing_data_timer = void 0;
-  now_playing_data_client = Titanium.Network.createHTTPClient();
-  artist_text = Titanium.UI.createLabel({
+  window.audio_stream = Titanium.Media.createAudioPlayer();
+  window.now_playing_track = new Track();
+  window.playing = false;
+  window.now_playing_data_timer = void 0;
+  window.now_playing_data_client = Titanium.Network.createHTTPClient();
+  window.artist_text = Titanium.UI.createLabel({
     className: "track_label"
   });
-  album_text = Titanium.UI.createLabel({
+  window.album_text = Titanium.UI.createLabel({
     className: "track_label"
   });
-  track_text = Titanium.UI.createLabel({
+  window.track_text = Titanium.UI.createLabel({
     className: "track_label"
   });
-  artwork_imageview = Titanium.UI.createImageView({
+  window.artwork_imageview = Titanium.UI.createImageView({
     top: 10,
     left: 30,
     width: 260,
     height: 260
   });
-  play_button = Titanium.UI.createImageView({
+  window.play_button = Titanium.UI.createImageView({
     top: 350,
     left: 10,
     width: 50,
     height: 50,
     image: "assets/images/playbutton.png"
   });
-  refresh_now_playing_data = function() {
-    var self, url;
-    self = this;
+  window.refresh_now_playing_data = function() {
+    var url;
     url = "http://nwarner.com/projects/sandbox/now_playing.json";
-    now_playing_data_client.onload = refresh_now_playing_data_success;
-    now_playing_data_client.onerror = refresh_now_playing_data_error;
-    now_playing_data_client.open("GET", url);
-    now_playing_data_client.send();
-    clearInterval(now_playing_data_timer);
-    return now_playing_data_timer = setTimeout(function() {
-      return refresh_now_playing_data();
+    Ti.ajax({
+      url: url,
+      success: function(response) {
+        window.now_playing_track = response.track;
+        log(window.now_playing_track);
+        return window.update_ui();
+      },
+      error: function(response) {
+        return alert("now_playing_track response error!");
+      }
+    });
+    clearInterval(window.now_playing_data_timer);
+    return window.now_playing_data_timer = setTimeout(function() {
+      return window.refresh_now_playing_data();
     }, 20000);
   };
-  refresh_now_playing_data_success = function() {
-    var response;
-    response = JSON.parse(now_playing_data_client.responseText);
-    now_playing_track = response.track;
-    return update_ui();
+  window.update_ui = function() {
+    window.artist_text.text = window.now_playing.artist;
+    window.album_text.text = window.now_playing.album;
+    window.track_text.text = window.now_playing.title;
+    return window.artwork_imageview.image = "assets/images/album_bg.png";
   };
-  refresh_now_playing_data_error = function() {};
-  update_ui = function() {
-    artist_text.text = now_playing.artist;
-    album_text.text = now_playing.album;
-    track_text.text = now_playing.title;
-    return artwork_imageview.image = "assets/images/album_bg.png";
+  window.play_stream = function() {
+    window.playing = true;
+    window.audio_stream.setUrl(app.config.urls.stream);
+    window.audio_stream.start();
+    return window.play_button.image = "assets/images/stopbutton.png";
   };
-  play_stream = function() {
-    playing = true;
-    audio_stream.setUrl(stream_url);
-    audio_stream.start();
-    return play_button.image = "assets/images/stopbutton.png";
+  window.stop_stream = function() {
+    window.playing = false;
+    window.audio_stream.stop();
+    return window.play_button.image = "assets/images/playbutton.png";
   };
-  stop_stream = function() {
-    playing = false;
-    audio_stream.stop();
-    return play_button.image = "assets/images/playbutton.png";
-  };
-  toggle_play_stream = function() {
-    if (playing) {
-      return stop_stream();
+  window.toggle_play_stream = function() {
+    if (window.playing) {
+      return window.stop_stream();
     } else {
-      return play_stream();
+      return window.play_stream();
     }
   };
-  window.add(artist_text);
-  window.add(album_text);
-  window.add(track_text);
-  window.add(artwork_imageview);
-  window.add(play_button);
-  play_button.addEventListener("click", function() {
-    return toggle_play_stream();
-  });
   window.update = function() {
-    return refresh_now_playing_data();
+    return window.refresh_now_playing_data();
   };
+  window.add(window.artist_text);
+  window.add(window.album_text);
+  window.add(window.track_text);
+  window.add(window.artwork_imageview);
+  window.add(window.play_button);
+  $(window.play_button).click(function() {
+    return window.toggle_play_stream();
+  });
   window.addEventListener("focus", function() {
     return window.update();
   });
